9809f610bed24d9424fb7c7ab82ea1dc777b6ecc
fernanda-souza1 <69874168+fernanda-souza1@users.noreply.github.com>
2020-09-17 14:08:53 -0300

    CALONI-7: criar o projeto tiologreplay2

---
+++ tools/tiologreplay2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiodb2
+{
+    class Container
+    {
+        static void Main(string[] args)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                /*<---------->*/
+
+            }
+        }
+    }
+}
